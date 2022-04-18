a2dcd183883189b76d03804297d6d56287325be4
Axel Vasquez <a.axelvc@gmail.com>
2022-04-18 16:18:26 -0500

    init: :tada: init proyect

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <main>
+      <Head>
+        <title>Axel&apos;s Portfolio</title>
+        <meta name="description" content="Awesome description" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Hello world!</h1>
+    </main>
+  )
+}
+
+export default Home
