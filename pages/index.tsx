4f62b4eb045371d26b28298164dce138208c3d8b
Axel Vasquez <a.axelvc@gmail.com>
2022-04-18 17:08:45 -0500

    build: :heavy_plus_sign: add tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </main>
   )
 }
